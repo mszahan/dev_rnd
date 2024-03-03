415c5e05bcbe416176d2a7a8872c7359a1364792
mszahan <mszahan1@gmail.com>
2024-03-04 02:39:36 +0600

    added a mini desing system with sass mixin

--- frontend/sass_testing/src/App.jsx
+++ frontend/sass_testing/src/App.jsx
@@ -1,9 +1,16 @@
 import styles from "./styles/home.module.scss";
 
+// const Button = ({ name }) => {
+//   return <button>{name}</button>;
+// };
+
 function App() {
   return (
     <div className={styles.main}>
       <h1 className={styles.title}>Hola from vite</h1>
+      <button className={styles.btnPrimary}>Primary</button>
+      <button className={styles.btnSecondary}>Secondary</button>
+      <button className={styles.btnSubmit}>Submit</button>
     </div>
   );
 }
