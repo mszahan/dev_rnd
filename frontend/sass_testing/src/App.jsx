a192670ec241f324bade55c0f9c11e844750d43b
mszahan <mszahan1@gmail.com>
2024-03-04 01:48:40 +0600

    configured sass with css module in vite react project

---
+++ frontend/sass_testing/src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles/home.module.scss";
+
+function App() {
+  return (
+    <div className={styles.main}>
+      <h1 className={styles.title}>Hola from vite</h1>
+    </div>
+  );
+}
+
+export default App;
