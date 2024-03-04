6e1fa06efb5ed03060f86f8fae3e1e3effff8c65
mszahan <mszahan1@gmail.com>
2024-03-05 02:35:12 +0600

    used mixin in different way and added extend

--- frontend/sass_testing/src/App.jsx
+++ frontend/sass_testing/src/App.jsx
@@ -8,9 +8,10 @@ function App() {
   return (
     <div className={styles.main}>
       <h1 className={styles.title}>Hola from vite</h1>
-      <button className={styles.btnPrimary}>Primary</button>
+      <button className={`${styles.btn} ${styles.btnPrimary}`}>Primary</button>
       <button className={styles.btnSecondary}>Secondary</button>
       <button className={styles.btnSubmit}>Submit</button>
+      <button className={styles.btnSuccess__newButton_active}>Success</button>
     </div>
   );
 }
