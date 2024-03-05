8ae4c6d35ad46e7c52911e2d748d0c0b24573168
mszahan <mszahan1@gmail.com>
2024-03-05 23:01:45 +0600

    made the show hide button dynamic

--- frontend/sass_testing/src/components/Parent.jsx
+++ frontend/sass_testing/src/components/Parent.jsx
@@ -2,21 +2,27 @@ import { useState } from "react";
 import styles from "../styles/parent.module.scss";
 
 export default function Parent() {
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(false);
   return (
     <div className={styles.parent}>
       <h2 className={styles.title}>Parent selector</h2>
-      {visible && (
-        <p className={styles.para}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis,
-          enim nulla dolore ipsam aspernatur velit quas tempora similique
-          voluptate quidem, omnis esse. Mollitia maxime voluptatem iste
-          doloremque dolore porro? Sed.
-        </p>
+      {visible ? (
+        <>
+          <p className={styles.para}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis,
+            enim nulla dolore ipsam aspernatur velit quas tempora similique
+            voluptate quidem, omnis esse. Mollitia maxime voluptatem iste
+            doloremque dolore porro? Sed.
+          </p>
+          <button onClick={() => setVisible(false)} className={styles.bton}>
+            Hide
+          </button>
+        </>
+      ) : (
+        <button onClick={() => setVisible(true)} className={styles.bton}>
+          Show
+        </button>
       )}
-      <button onClick={() => setVisible(!visible)} className={styles.bton}>
-        Hide/show
-      </button>
     </div>
   );
 }
